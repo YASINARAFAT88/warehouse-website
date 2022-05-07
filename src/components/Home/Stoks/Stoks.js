dd5598135c37f09e44fa1400e30d0e403c9e0638
HP 840 <yasin80545859@.com>
2022-05-07 14:09:51 +0600

    add management item

--- src/components/Home/Stoks/Stoks.js
+++ src/components/Home/Stoks/Stoks.js
@@ -1,22 +1,27 @@
 import React, { useContext } from 'react';
 import { HooksContext } from '../../../App';
+import { useNavigate } from 'react-router-dom';
 import Stock from '../Stock/Stock';
 import './Stoks.css'
 
 const Stoks = () => {
+    const naviGate = useNavigate()
     const stocks = useContext(HooksContext)
-    
+
     return (
         <div id='stocks'>
             <h1 className='stocks-title text-center'>Warehouse available stoks</h1>
             <div className="stocks-container">
-            {
-                stocks.slice(0, 6).map(stock => <Stock
-                key={stock._id}
-                stock={stock}
-                ></Stock>)
-            }  
-            </div>  
+                {
+                    stocks.slice(0, 6).map(stock => <Stock
+                        key={stock._id}
+                        stock={stock}
+                    ></Stock>)
+                }
+            </div>
+            <div className='btn-div'>
+                    <button onClick={() => naviGate('/manageinventory')} className='review-btn text-center fw-bold bg-dark'>See All Items</button>
+                </div>
         </div>
     );
 };
